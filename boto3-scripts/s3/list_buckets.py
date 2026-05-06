045ed7c0f69cac58afe695bd7b3dd7479d7c7c05
T0NYF0V3 <anthonyfolgueras@gmail.com>
2026-05-06 01:47:16 -0600

    Enhance S3 bucket listing output format
    Added formatted output for S3 bucket listing.

--- boto3-scripts/s3/list_buckets.py
+++ boto3-scripts/s3/list_buckets.py
@@ -1,7 +1,14 @@
 import boto3
 
+# Create S3 client
 s3 = boto3.client('s3')
 
+# Retrieve bucket list
 response = s3.list_buckets()
 
-print(response)
+print("\n========== S3 BUCKETS ==========\n")
+
+for bucket in response['Buckets']:
+    print(f"Bucket Name: {bucket['Name']}")
+
+print("\n================================\n")
