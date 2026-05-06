8818867b2d9d58a9b26df774a01063dad3282446
T0NYF0V3 <anthonyfolgueras@gmail.com>
2026-05-06 01:46:34 -0600

    Update list_ec2.py

--- boto3-scripts/ec2/list_ec2.py
+++ boto3-scripts/ec2/list_ec2.py
@@ -1,7 +1,26 @@
 import boto3
 
-ec2 = boto3.client('ec2')
+# Create EC2 client
+ec2 = boto3.client(
+    'ec2',
+    region_name='us-east-1'
+)
 
+# Retrieve EC2 instances
 response = ec2.describe_instances()
 
-print(response)
+print("\n========== EC2 INSTANCES ==========\n")
+
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+
+        instance_id = instance['InstanceId']
+        state = instance['State']['Name']
+        instance_type = instance['InstanceType']
+
+        print(f"Instance ID   : {instance_id}")
+        print(f"State         : {state}")
+        print(f"Instance Type : {instance_type}")
+        print("-" * 40)
+
+print("\n===================================\n")
